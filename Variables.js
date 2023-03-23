82fb4defa6539c9fbfdfe945e6c1acf85d484247
Matias <matiasaguirree11@gmail.com>
2023-03-23 15:24:49 -0300

    segundo commit

---
+++ Variables.js
@@ -0,0 +1,25 @@
+// strings
+
+var Aguirre = 'Matias';
+
+
+// numeros 
+
+var numeroEntero = 18;
+var numeroNegativo = -5;
+var numeroDecimal = 15.3;
+
+// Booleanos ( VERDADERO O FALSO)
+
+var estoyEntendiendo = true;
+var meAburro = false;
+
+// Undefined
+
+var Indefinida;
+
+// Null
+
+var nula = null;
+
+console.log(numeroEntero)
