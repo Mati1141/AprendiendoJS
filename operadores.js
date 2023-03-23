35addf8a8e7a13952bc30da94a4b3b27f5cea815
Matias <matiasaguirree11@gmail.com>
2023-03-23 15:51:35 -0300

    Operadores Aritmeticos

---
+++ operadores.js
@@ -0,0 +1,47 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+
+
+
+//CONCATENAR
+
+console.log("hola " + "Matias");
+
+var nombre = "Matias";
+var saludo = "Hola " + nombre;
+console.log(saludo)
+
+
+
+
+
+
+
+var division = 25 % 5;
+
+console.log(division)
+
+if (division === 0){
+    console.log("son divisibles")
+}
+else{
+    console.log("no son divisibles")
+}
+
+
